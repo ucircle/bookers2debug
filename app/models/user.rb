a6af3e7ecd8e3b3b92eb637bf47d353edca9a844
EC2 Default User <ec2-user@ip-172-31-43-62.ap-northeast-1.compute.internal>
2023-07-30 06:55:14 +0000

    search

--- app/models/user.rb
+++ app/models/user.rb
@@ -32,7 +32,22 @@ end
 #フォローしていればtrueを返す
 def following?(user)
   following_users.include?(user)
-end	 
+end
+
+def self.looks(search, word)
+    if search == "perfect_match"
+      @user = User.where("name LIKE?", "#{word}")
+    elsif search == "forward_match"
+      @user = User.where("name LIKE?","#{word}%")
+    elsif search == "backward_match"
+      @user = User.where("name LIKE?","%#{word}")
+    elsif search == "partial_match"
+      @user = User.where("name LIKE?","%#{word}%")
+    else
+      @user = User.all
+    end
+end
+
   
 
   def get_profile_image(width, height)
--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,7 @@ Rails.application.routes.draw do
   end
 
   get "home/about" => "homes#about"
+  get "search" => "searches#search"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 end
