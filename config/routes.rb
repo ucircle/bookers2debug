e96f307cde095c59f0cb5d002bb3f8938e2db1a1
EC2 Default User <ec2-user@ip-172-31-43-62.ap-northeast-1.compute.internal>
2023-08-01 09:31:47 +0000

    favodouki

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  get 'relationships/create'
-  get 'relationships/destroy'
+
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root :to =>"homes#top"
-  devise_for :users
+
 
 
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
